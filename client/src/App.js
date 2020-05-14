f6c3024bed84dfb7eddeec56fd08bdc62245b5d1
SPFlores <sflores92886@gmail.com>
2020-05-14 16:11:58 -0700

    placeholder navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Landing from './pages/Landing'
+import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const App = _ => {
   return (
     <>
       <Router>
+        <Navbar />
         <Route exact path='/' render={_ => (<Landing />)} />
       </Router>
     </>
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = _ => {
+  return (
+    <div class='navbar'>
+      This is a navbar
+    </div>
+  )
+}
+
+export default Navbar
