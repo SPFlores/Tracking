97492c3315c5a59c1e3f62193c98d0fa73f96ac5
SPFlores <sflores92886@gmail.com>
2020-05-15 17:41:16 -0700

    drawer setupon menu

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,12 +4,15 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import { makeStyles } from '@material-ui/core/styles'
-import MenuIcon from '@material-ui/icons/Menu'
+import { Menu, Home, RestaurantMenu } from '@material-ui/icons'
 import Drawer from '@material-ui/core/Drawer'
 import List from '@material-ui/core/List'
 import Divider from '@material-ui/core/Divider'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -29,12 +32,13 @@ const useStyles = makeStyles(theme => ({
     width: 250
   },
   appBar: {
-    background: '#362c36'
+    background: '#00b9cd'
   }
 }))
 
 const Navbar = _ => {
   const classes = useStyles()
+
   const [drawerState, setDrawerState] = useState({
     left: false
   })
@@ -43,15 +47,14 @@ const Navbar = _ => {
     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
       return
     }
-
     setDrawerState({ ...drawerState, [side]: open })
   }
 
-  const handleLogOut = _ => {
-    localStorage.removeItem('token')
-    localStorage.removeItem('user')
-    localStorage.removeItem('admin')
-  }
+  // const handleLogOut = _ => {
+  //   localStorage.removeItem('token')
+  //   localStorage.removeItem('user')
+  //   localStorage.removeItem('admin')
+  // }
 
   drawerState.sideList = side => (
     <div
@@ -61,25 +64,21 @@ const Navbar = _ => {
       onKeyDown={drawerState.toggleDrawer(side, false)}
     >
       <List>
-        <ListItem>
-          <i className='material-icons'>library_books</i>
-          <Link to='/' className='drawerLink'><ListItemText primary='Latest News' /></Link>
+        <ListItem button>
+          <ListItemIcon>
+            <Home />
+          </ListItemIcon>
+          <Link to='/' className='drawerLink'>
+            <ListItemText primary='Home' />
+          </Link>
         </ListItem>
-        <ListItem>
-          <i className='material-icons'>new_releases</i>
-          <Link to='/latest' className='drawerLink'><ListItemText primary='In Theaters' /></Link>
-        </ListItem>
-        <ListItem>
-          <i className='material-icons'>movie</i>
-          <Link to='/toprated' className='drawerLink'><ListItemText primary='Top Movies' /></Link>
-        </ListItem>
-        <ListItem>
-          <i className='material-icons'>local_movies</i>
-          <Link to='/genres' className='drawerLink'><ListItemText primary='Genres' /></Link>
-        </ListItem>
-        <ListItem>
-          <i className='material-icons'>search</i>
-          <Link to='/search' className='drawerLink'><ListItemText primary='Search' /></Link>
+        <ListItem button>
+          <ListItemIcon>
+            <RestaurantMenu />
+          </ListItemIcon>
+          <Link to='/' className='drawerLink'>
+            <ListItemText primary='Menu' />
+          </Link>
         </ListItem>
       </List>
       <Divider />
@@ -101,7 +100,8 @@ const Navbar = _ => {
               </ListItem>
               <ListItem>
                 <i className='material-icons'>exit_to_app</i>
-                <Link to='/' className='drawerLink'><ListItemText primary='Log Out' onClick={handleLogOut} />
+                <Link to='/' className='drawerLink'><ListItemText primary='Log Out' />
+                  {/* onClick={handleLogOut} /> */}
                 </Link>
               </ListItem>
 
@@ -118,7 +118,6 @@ const Navbar = _ => {
             </>
         }
       </List>
-
     </div>
   )
 
@@ -127,8 +126,17 @@ const Navbar = _ => {
       <AppBar position='static' className={classes.appBar}>
         <Toolbar>
           <IconButton edge='start' className={classes.menuButton} color='inherit' aria-label='Menu' onClick={drawerState.toggleDrawer('left', true)}>
-            <MenuIcon />
+            <Menu />
           </IconButton>
+          <Typography varient='h6' classname={classes.title}>
+            Tracking App Title
+          </Typography>
+          <Button color='inherit'>
+            Login
+          </Button>
+          <Button color='inherit'>
+            Logout
+          </Button>
         </Toolbar>
       </AppBar>
       <Drawer open={drawerState.left} onClose={drawerState.toggleDrawer('left', false)}>
