686fcfa94ccc6cebb909ebabc0e5b670b8614748
SPFlores <sflores92886@gmail.com>
2020-05-19 14:29:03 -0700

    user page added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Landing from './pages/Landing'
 import Menu from './pages/Menu'
 import Food from './pages/Food'
+import User from './pages/User'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
@@ -14,6 +15,7 @@ const App = _ => {
         <Route exact path='/' render={_ => (<Landing />)} />
         <Route path='/menu' render={_ => (<Menu />)} />
         <Route path='/food' render={_ => (<Food />)} />
+        <Route path='/user' render={_ => (<User />)} />
         <Footer />
       </Router>
     </>
---
+++ client/src/pages/User/User.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const User = _ => {
+  return (
+    <p>This is a sample user page</p>
+  )
+}
+
+export default User
